4a3614e7d7bb8b8f1b87dbe9da8b38a67b998549
minty <jason.c.willey@gmail.com>
2018-07-23 00:18:20 -0400

    added footer information and cleaned up header

--- locator/forms.py
+++ locator/forms.py
@@ -3,10 +3,10 @@ from django import forms
 
 
 class LocatorForm(forms.ModelForm):
-    city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder': 'City ','class': 'messageText', 'size':'25'}))
-    ip_address = forms.GenericIPAddressField(required=True, widget=forms.widgets.NumberInput(attrs={'placeholder': 'Entrer IP Address Here','class': 'messageText', 'size':'16'}))
-    longitude = forms.DecimalField(required=True, widget=forms.widgets.NumberInput(attrs={'placeholder': 'Entrer IP Longitude Here','class': 'messageText', 'size':'16'}))
-    latitude = forms.DecimalField(required=True, widget=forms.widgets.NumberInput(attrs={'placeholder': 'Entrer IP Latitude Here','class': 'messageText', 'size':'16'}))
+    city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder': 'Current City Name ','class': 'messageText', 'size':'25'}))
+    ip_address = forms.GenericIPAddressField(required=True, widget=forms.widgets.NumberInput(attrs={'placeholder': 'IP Address','class': 'messageText', 'size':'16'}))
+    longitude = forms.DecimalField(required=True, widget=forms.widgets.NumberInput(attrs={'placeholder': 'Longitude Coordinate','class': 'messageText', 'size':'16'}))
+    latitude = forms.DecimalField(required=True, widget=forms.widgets.NumberInput(attrs={'placeholder': 'Latitude Coordinate','class': 'messageText', 'size':'16'}))
 
     class Meta:
         model = Location
