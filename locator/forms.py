bd1b1264b04acae9a052d462731c269d472ba64f
minty <jason.c.willey@gmail.com>
2018-07-26 20:25:08 -0400

    added install instructions to README.md

--- locator/forms.py
+++ locator/forms.py
@@ -4,10 +4,9 @@ from django import forms
 
 class LocatorForm(forms.ModelForm):
     city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder': 'Current City Name ','class': 'messageText', 'size':'25'}))
-    ip_address = forms.GenericIPAddressField(required=True, widget=forms.widgets.NumberInput(attrs={'placeholder': 'IP Address','class': 'messageText', 'size':'16'}))
     longitude = forms.DecimalField(required=True, widget=forms.widgets.NumberInput(attrs={'placeholder': 'Longitude Coordinate','class': 'messageText', 'size':'16'}))
     latitude = forms.DecimalField(required=True, widget=forms.widgets.NumberInput(attrs={'placeholder': 'Latitude Coordinate','class': 'messageText', 'size':'16'}))
 
     class Meta:
         model = Location
-        fields = ('city', 'ip_address', 'longitude', 'latitude')
+        fields = ('city', 'longitude', 'latitude')
